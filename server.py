f80284b3b8dc783d2049d880d3b3768acc13fb44
eChris <cc.emenike@gmail.com>
2022-06-12 13:43:38 +0100

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello, " + name + "!\n"
+    message = "Welcome to your AWS App Runner Application Mr., " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
